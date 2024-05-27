a3f5353b4f93d565e335a86bdde86b2f2c3bd991
user <irin-shkuratov8@yandex.ru>
2024-05-27 17:08:55 +0300

    /

--- static/js/test_div.js
+++ static/js/test_div.js
@@ -17,47 +17,67 @@ const label_3 = document.querySelector('#label_3');
 const answer_1 = document.querySelector('#answer_1');
 const answer_2 = document.querySelector('#answer_1');
 const answer_3 = document.querySelector('#answer_3');
+const btn_3 = document.querySelector('#btn_3');
 
 const array_1 = ['Почему Вы плачете?', 'Всё идёт не так', 'Кто-то обижает', 'Чувствую моральную боль'];
+const array_2 = ['Что вас огорчает?', 'Работа', 'Учёба', 'Жизнь'];
+
+const object_1 = {1: ['Как часто у вас такое состояние?', 'Каждый день', 'Каждую неделю', 'Каждый месяц'], 2: ['Как скоро проходит это состояние?', 'Через день', 'Через неделю', 'Вообще не проходит!']};
+const object_2 = {1: ['Как часто у вас такое состояние?', 'Каждый день', 'Каждую неделю', 'Каждый месяц'], 2: ['Что приводит вас к такому состоянию?', 'Неудачи', 'Всё идёт не по плану', 'Сам себе не нравлюсь']}
+let default_object;
+let count = 0;
 
 const func = function(array) {
     test_div.classList.add('display_none');
     test_div.classList.remove('display_none');
 
-    const p_1 = document.createElement('p');
-    const p_2 = document.createElement('p');
-    const p_3 = document.createElement('p');
-    const p_4 = document.createElement('p');
 
     if (array[0] != undefined) {
-        p_1.innerText = array[0];
+        test_question.innerText = array[0];
     }
 
     if (array[1] != undefined) {
-        p_2.innerText = array[1];
+        label_1.innerText = array[1];
     }
 
     if (array[2] != undefined) {
-    p_3.innerText = array[2];
+        label_2.innerText = array[2];
 
     }
 
     if (array[3] != undefined) {
-        p_4.innerText = array[3];
+        label_3.innerText = array[3];
     }
-
-    test_question.append(p_1);
-    label_1.append(p_2);
-    label_2.append(p_3);
-    label_3.append(p_4);
 }
 
-btn_smiley_1.addEventListener('click', function() {
-    func(array_1);
-});
-btn_smiley_2.addEventListener('click', function() {
-    func(2);
-});
+btn_3.onclick = function() {
+    count ++;
+    const list = default_object[count];
+    if (list[0] != undefined) {
+        test_question.innerText = list[0];
+    }
+
+    if (list[1] != undefined) {
+        label_1.innerText = list[1];
+    }
+
+    if (list[2] != undefined) {
+        label_2.innerText = list[2];
+
+    }
+
+    if (list[3] != undefined) {
+        label_3.innerText = list[3];
+    }
+
+};
+
+btn_smiley_1.onclick = function() {
+    func(array_1), default_object = object_1;
+};
+btn_smiley_2.onclick = function() {
+    func(array_2), default_object = object_2;
+};
 btn_smiley_3.addEventListener('click', function() {
     func(3);
 });
