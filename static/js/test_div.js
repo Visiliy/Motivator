61b8a88079156f7f9c95407071786cd62ee298c6
user <irin-shkuratov8@yandex.ru>
2024-05-28 13:07:45 +0300

    /

--- static/js/test_div.js
+++ static/js/test_div.js
@@ -8,6 +8,7 @@ const btn_smiley_7 = document.querySelector('#click_7');
 
 const test_div = document.querySelector('#test_div');
 const span_form = document.querySelector('#span_form');
+const card_form = document.querySelector('#card_form');
 
 const test_question = document.querySelector('#test_question');
 const label_1 = document.querySelector('#label_1');
@@ -18,6 +19,10 @@ const answer_1 = document.querySelector('#answer_1');
 const answer_2 = document.querySelector('#answer_1');
 const answer_3 = document.querySelector('#answer_3');
 const btn_3 = document.querySelector('#btn_3');
+
+const string_1 = 'Не волнуйтесь, Вы обязательно поправитесь! Главное - не падайте духом! Улыбнитесь, хорошее настроение помогает быстрее выздороветь!';
+const string_2 = 'Мы рады, что у Вас прекрасное настороение! Желаем, чтобы оно оставалось как можно дольше!';
+const string_3 = 'Как хорошо, что у Вас праздничное настроение! Отличное настроение продлевает жизнь, делает её яркой и незабываемой!';
 
 const array_1 = ['Почему Вы плачете?', 'Всё идёт не так', 'Кто-то обижает', 'Чувствую моральную боль'];
 const array_2 = ['Что вас огорчает?', 'Работа', 'Учёба', 'Жизнь'];
@@ -50,8 +55,38 @@ const func = function(array) {
     }
 }
 
+const func_2 = function(string) {
+    test_div.classList.add('display_none');
+    card_form.classList.remove('display_none');
+
+    const h2 = document.createElement('h2');
+    const h4 = document.createElement('h4');
+
+    h2.innerText = 'Пожелания';
+    h2.classList.add('h2_form_div');
+
+    h4.innerText = string;
+    h4.classList.add('h2_form_div');
+    h4.classList.add('margin_top');
+
+    card_form.append(h2);
+    card_form.append(h4);
+
+}
+
 btn_3.onclick = function() {
+    let rate_value;
+    if (document.querySelector('#answer_1').checked) {
+        rate_value = document.querySelector('#answer_1').value;
+    }
+    if (document.querySelector('#answer_2').checked) {
+        rate_value = document.querySelector('#answer_2').value;
+    }
+    if (document.querySelector('#answer_3').checked) {
+        rate_value = document.querySelector('#answer_3').value;
+    }
     count ++;
+    console.log(rate_value);
     const list = default_object[count];
     if (list[0] != undefined) {
         test_question.innerText = list[0];
@@ -78,9 +113,9 @@ btn_smiley_1.onclick = function() {
 btn_smiley_2.onclick = function() {
     func(array_2), default_object = object_2;
 };
-btn_smiley_3.addEventListener('click', function() {
-    func(3);
-});
+btn_smiley_3.onclick = function() {
+    func_2(string_1);
+};
 btn_smiley_1.addEventListener('click', function() {
     func(1);
 });
@@ -90,9 +125,9 @@ btn_smiley_4.addEventListener('click', function() {
 btn_smiley_5.addEventListener('click', function() {
     func(5);
 });
-btn_smiley_6.addEventListener('click', function() {
-    func(6);
-});
-btn_smiley_7.addEventListener('click', function() {
-    func(7);
-});
+btn_smiley_6.onclick = function() {
+    func_2(string_2);
+};
+btn_smiley_7.onclick = function() {
+    func_2(string_3);
+};
