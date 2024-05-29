c3cf0f3ecb8d034e06a01e601416f7bc7742b6a0
user <irin-shkuratov8@yandex.ru>
2024-05-29 17:48:47 +0300

    /

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('first_page/main.html')
+    return render_template('main.html')
+
+
+@app.route('/entrance', methods=['GET', 'POST'])
+def entrance():
+    if request.method == 'GET':
+        return render_template('entrance.html')
 
 
 if __name__ == "__main__":
